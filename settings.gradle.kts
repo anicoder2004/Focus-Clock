0d3940020c7b3170128ce915f32e39f38b11f414
Anicoder2004 <aniklancer2004@gmail.com>
2026-08-16 03:37:05 +0600

    Gradle Config

---
+++ settings.gradle.kts
@@ -0,0 +1,39 @@
+rootProject.name = "FocusClock"
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        maven { url = uri("https://oss.sonatype.org/content/repositories/releases/") }
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        maven { url = uri("https://repo1.maven.org/maven2/") }
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+include(":androidApp")
+include(":desktopApp")
+include(":shared")
